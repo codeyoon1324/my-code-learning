a88199504bf64f8302ebd44e92a386cc670f6b76
codeyoon1324 <116942209+codeyoon1324@users.noreply.github.com>
2022-11-09 16:14:47 +0900

    Create How to loop but using 'while'.c
    I uploaded how to loop.
    But that was using 'for' I guess.
    So 'while' command is quite simple but hard to use in some conditions.
    Enjoy!
                                                                                                                   by.codeyoon1324

---
+++ How to loop but using 'while'.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+int i; //variable that count the numbers.
+  i=1;
+  while(i<=1000)//execute if 'i' is under or same as 1000
+    {
+      printf("%d\n", i);
+      i++;
+    }
+}
